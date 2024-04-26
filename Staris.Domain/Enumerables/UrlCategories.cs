c39f05d6221c4a89fb1d596b5dddee42be0a83aa
Jonathan Bockorny <jonathan.igor.pereira@hotmail.com>
2024-04-26 11:15:45 -0300

    códigos iniciais

---
+++ Staris.Domain/Enumerables/UrlCategories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staris.Domain.Enumerables;
+public enum EnumUrlCategories
+{
+    People = 1,
+    planets = 2,
+    films = 3,
+    vehicles = 4,
+    Species = 5,
+    Starships = 6
+}
